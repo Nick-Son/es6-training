72c38c4a553f91df250ad63494c24eb4e7f6bec6
Nick Sonner <nicksonner@gmail.com>
2018-03-13 14:22:57 +1100

    add src folder, config.js

---
+++ 13 - JavaScript Modules and Using npm/es6modules/src/config.js
@@ -0,0 +1,12 @@
+export const apiKey = 'abc123';
+
+export const url = 'http://wesbos.com';
+
+export function sayHi(name) {
+  return `Hi there ${name}!`
+};
+
+const age = 34
+const game = 'Guild Wars 2'
+
+export { age, game }
