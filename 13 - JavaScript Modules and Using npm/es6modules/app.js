df5144f0eeafb7fe32c4665535258e891fca9551
Nick Sonner <nicksonner@gmail.com>
2018-03-13 14:48:19 +1100

    up to exercise 48

--- 13 - JavaScript Modules and Using npm/es6modules/app.js
+++ 13 - JavaScript Modules and Using npm/es6modules/app.js
@@ -2,3 +2,10 @@ import { uniq } from 'lodash';
 import insane from 'insane';
 import jsonp from 'jsonp';
 import { apiKey, url } from './src/config';
+
+import User, { createUrl, gravatar } from './src/user';
+
+const nick = new User('Nick Sonner', 'nicksonner@gmail.com', 'nicksonner.io')
+
+const profile = createUrl(nick.name)
+const avatar = gravatar(nick.email)
