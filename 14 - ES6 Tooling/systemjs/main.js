1e58db5f933ab8a1ba09e49f73d7785eed8879a4
Nick Sonner <nicksonner@gmail.com>
2018-03-14 14:01:55 +1100

    up to exercise 49

--- 14 - ES6 Tooling/systemjs/main.js
+++ 14 - ES6 Tooling/systemjs/main.js
@@ -1,1 +1,5 @@
-console.log('Hey, it works!');
+import { sum, kebabCase } from 'npm:lodash';
+import { addTax } from './checkout';
+
+console.log(kebabCase('Hey It Works'));
+console.log(addTax(24, 1.2));
